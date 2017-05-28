a538079d2296ba39fb7e24dde8082d74e43763c7
Nicolas Blanc <nicolasblanc360@gmail.com>
2017-05-28 22:33:08 +0200

    End of 1.2_2, Condifence intervals are completed

--- Synth_errorRate.R
+++ Synth_errorRate.R
@@ -9,12 +9,11 @@ error_test <-  matrix(0, length(donn_list), 20);
 
 #start of algorithm 
 for (i in 1:length(donn_list)) {
-  print(paste("i = " ,i));
   data <- as.data.frame(donn_list[])
   X <- data[,1:2];
   z <- data[,3];
   for (j in 1:20 ){
-    print(paste("j = " ,j));
+
   donn.sep <- separ1(X, z) #using separ1.R function 
   
   #parting in learning and test sets 
@@ -46,6 +45,21 @@ average_error_rate <- matrix(0, nrow = length(donn_list), ncol = 2);
 average_error_rate[,1] = apply(error_train, 1, mean);
 average_error_rate[,2] = apply(error_test, 1, mean);
   
-  
+#Confidence interval 
 
+#we consider with alpha = 0.05
+#We use : CI = [p(mean) - fract(0.975*s/sqrt(n)), p(mean) + fract(0.975*s/sqrt(n))]
+
+ptrain <- as.matrix(average_error_rate[, 1]);
+ptest <- as.matrix(average_error_rate[, 2]);
+frac = 1.96;
+
+CI_tab <- matrix(0, nrow = nrow(average_error_rate), ncol = 4); #4 because of two values for each interval 
+for (i in 1:nrow(average_error_rate)){
+  CI_tab[i, 1] <- ptrain[i] - frac * sqrt((ptrain[i]*(1-ptrain[i]))/nrow(as.data.frame(donn_list[i]))); #lower bound for CI on train set
+  CI_tab[i, 2] <- ptrain[i] + frac * sqrt((ptrain[i]*(1-ptrain[i]))/nrow(as.data.frame(donn_list[i]))); #upper bound for CI on train set
+  CI_tab[i, 3] <- ptest[i] - frac * sqrt((ptest[i]*(1-ptest[i]))/nrow(as.data.frame(donn_list[i]))); #lower bound for CI on test set
+  CI_tab[i, 4] <- ptest[i] + frac * sqrt((ptest[i]*(1-ptest[i]))/nrow(as.data.frame(donn_list[i]))); #upper bound for CI on test set
+  
+}
 
